aec6f71ce13d1aa4fca00d7dc3fbae56ebc4373f
JaimeRios <shuin666@gmail.com>
2016-10-01 17:20:45 -0500

    Version con navegacion 1.0 rama jaime -Se agrego el boton registrar respuesta. -Se agrega el metodo que es llamado por este boton. -Se cargan unas respuestas en una lista al cargar la ventana de respuestas.
    En la ventana agregarTema
    -Se agrego el metodo que llama el boton agregartema

--- app/src/main/java/com/android/jshuin/todorespuestas/AgregarTema.java
+++ app/src/main/java/com/android/jshuin/todorespuestas/AgregarTema.java
@@ -9,9 +9,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AgregarTema extends AppCompatActivity {
+
+    @BindView(R.id.agregarTematituloTema)
+    EditText titulo;
+    @BindView(R.id.agregarTemapregunta)
+    EditText pregunta;
+    @BindView(R.id.agregarTemacorreo)
+    EditText correo;
+    @BindView(R.id.agregarTemaButtom)
+    Button agregartema;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,7 @@ public class AgregarTema extends AppCompatActivity {
         setContentView(R.layout.activity_agregar_tema);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);{}
+        ButterKnife.bind(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +73,22 @@ public class AgregarTema extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //metodo llamado por presionar el boton agregar tema
+    @OnClick(R.id.agregarTemaButtom)
+    public void enviarTema(){
+        if(titulo.getText().toString().matches("")||pregunta.getText().toString().matches("")||
+                correo.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(),"Por favor ingrese la informacion de todos los campos",Toast.LENGTH_SHORT).show();
+        }else{
+            //Envie la informacion a la base de datos aqui
+            Intent irAListaPreguntas = new Intent(AgregarTema.this,ListaPreguntas.class);
+            irAListaPreguntas.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(irAListaPreguntas);
+            finish();
+        }
+
     }
 
 }
