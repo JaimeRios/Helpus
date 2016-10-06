20af175db0d8c8fda3593c7e8f045593912bef7e
jeffveleze <jeffrsk8@hotmail.com>
2016-10-05 21:28:53 -0500

    - Se adiciona nuevo Splash

--- app/src/main/java/com/android/jshuin/todorespuestas/RegistrarUsuario.java
+++ app/src/main/java/com/android/jshuin/todorespuestas/RegistrarUsuario.java
@@ -60,19 +60,10 @@ public class RegistrarUsuario extends AppCompatActivity {
         setContentView(R.layout.activity_registrar_usuario);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ButterKnife.bind(this);
-
-        requestQueue= Volley.newRequestQueue(this);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ButterKnife.bind(this);
+        requestQueue= Volley.newRequestQueue(this);
 
     }
 
